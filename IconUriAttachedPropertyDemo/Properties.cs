2abf70ac448507d3d9133cac1b43d00a89b8f7d5
Justin Liu <justin.xin.liu@gmail.com>
2015-10-14 16:40:36 +1100

    Answer to http://stackoverflow.com/questions/33009277/how-to-bind-the-sourceuri-a-bitmapicon-to-radiobutton-in-windows-10-uwp-apps/33011522?noredirect=1#comment54048250_33011522

---
+++ IconUriAttachedPropertyDemo/Properties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace IconUriAttachedPropertyDemo
+{
+    public class Properties
+    {
+        public static Uri GetIconUri(DependencyObject obj)
+        {
+            return (Uri)obj.GetValue(IconUriProperty);
+        }
+
+        public static void SetIconUri(DependencyObject obj, Uri value)
+        {
+            obj.SetValue(IconUriProperty, value);
+        }
+
+        public static readonly DependencyProperty IconUriProperty =
+            DependencyProperty.RegisterAttached("IconUri", typeof(Uri), typeof(Properties), new PropertyMetadata(null));
+    }
+}
